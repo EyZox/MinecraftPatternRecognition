435a6b8bdae8857b3dbdd04f352f66d4058402e1
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2014-02-08 21:08:53 +0100

    Adds Icons to Select and Add buttons

--- MinecraftPatternGUI/src/fr/eyzox/minecraftPattern/gui/action/ActionPanel.java
+++ MinecraftPatternGUI/src/fr/eyzox/minecraftPattern/gui/action/ActionPanel.java
@@ -1,27 +1,32 @@
 package fr.eyzox.minecraftPattern.gui.action;
 
 import java.awt.FlowLayout;
+import java.awt.Insets;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.TreeMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
 
 @SuppressWarnings("serial")
 public class ActionPanel extends JPanel implements Observer{
 
-	private class JActionRadioButton extends JRadioButton {
+	private class JActionRadioButton extends JToggleButton {
 		private Action action;
 		
 		public JActionRadioButton(Action action) {
 			this.action = action;
+			this.setMargin(new Insets(0, 0, 0, 0));
 		}
 	}
 	
@@ -40,8 +45,8 @@ public class ActionPanel extends JPanel implements Observer{
 			actionMap.put(a, new JActionRadioButton(a));
 		}
 		
-		actionMap.get(Action.SELECT).setText("cursor");
-		actionMap.get(Action.ADDorREMOVE).setText("+/-");
+		actionMap.get(Action.SELECT).setText("/data/cursor.png");
+		actionMap.get(Action.ADDorREMOVE).setText("/data/addorremove.png");
 		
 		actionGroup = new ButtonGroup();
 		for(Action a : actionMap.keySet()) {
@@ -62,6 +67,20 @@ public class ActionPanel extends JPanel implements Observer{
 		}
 		
 		actionMap.get(Action.SELECT).setSelected(true);
+		setIcons();
+	}
+
+	private void setIcons() {
+		for(Action a : actionMap.keySet()) {
+			JActionRadioButton b = actionMap.get(a);
+			try {
+				b.setIcon(new ImageIcon(ImageIO.read(getClass().getResource(b.getText()))));
+				b.setText("");
+			} catch (IOException e) {
+				System.err.println("Unable to load "+b.getText());
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
